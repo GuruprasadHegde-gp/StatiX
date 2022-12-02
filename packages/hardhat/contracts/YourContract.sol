e001999a2376d9e8b8ab8a8a5b6bb7130d86515d
GuruprasadHegde-gp <101167708+GuruprasadHegde-gp@users.noreply.github.com>
2022-12-03 02:54:46 +0530

    Update YourContract.sol
    Added alll the functionalites for the user

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,67 @@
 pragma solidity >=0.8.0 <0.9.0;
+
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+error not_a_canceler(); // a custom error is declared
 
-contract YourContract {
+contract dev_staking {
+    address[] dev_address; // array of address to store the address of the developer(help seeker)
+    mapping(address => uint256) dev_to_reward; //mapping which stores the reward listed by each donor for his
+    address immutable help_deployer;
 
-  event SetPurpose(address sender, string purpose);
+    constructor() {
+        help_deployer = msg.sender;
+    }
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    function req_help() public payable {
+        dev_address.push(msg.sender); // address of the help seeker is stored into the array
+        dev_to_reward[msg.sender] = msg.value; //The reward Listed by the developer is stored into  his respective address
+    }
 
-  constructor() payable {
-    // what should we do on deploy?
-  }
+    //The above basic function posts the help along with the staked reward
 
-  function setPurpose(string memory newPurpose) public payable {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
+    function cancel_help() public {
+        // If the user wants to cancel the contract then this function is called
+        dev_to_reward[msg.sender] = 0; // All the reward is set to 0
+        uint help_cancel_ind = help_cancel_index(msg.sender); // calls the help_cancel_index function to get the index of cancelers address
+        delete_helper(help_cancel_ind); //delete_helper function is called
+        withdraw_cancelled_price(); //Returns the amount staked by the user
+    }
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    function help_cancel_index(
+        address cancelers_address
+    ) public view returns (uint256) {
+        uint256 cancelers_index;
+        for (uint i = 0; i <= dev_address.length; i++) {
+            if (dev_address[i] == cancelers_address) {
+                cancelers_index = i;
+                return cancelers_index;
+            }
+        }
+    }
+
+    function delete_helper(uint256 index) internal {
+        //This function ndeletes the cancelled helpers address and removes it totally from the array
+        if (index >= dev_address.length) return;
+
+        for (uint i = index; i < dev_address.length - 1; i++) {
+            dev_address[i] = dev_address[i + 1];
+        }
+        dev_address.pop();
+    }
+
+    function withdraw_cancelled_price() public canceler_only {
+        // if this function is called then the amount which was deployed by the help seeker will be reverted back to him
+        (bool callStatus, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callStatus, "Transaction Failed due to some Error's");
+    }
+
+    modifier canceler_only() {
+        if (msg.sender != help_deployer) {
+            revert not_a_canceler();
+        }
+        _;
+    }
 }
